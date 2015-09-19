908f6674496d0a82377f7c9dc0606a5e26414428
msangha <manjot@mit.edu>
2015-09-19 09:20:48 -0700

    XCode Project

---
+++ ARGameEngine/ARGameEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGameEngine
+//
+//  Created by Manjot Sangha on 2015-09-19.
+//  Copyright (c) 2015 Manjot Sangha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
