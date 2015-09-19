5af6ef14e38c890b8445aac92abbd02b14552b5a
msangha <manjot@mit.edu>
2015-09-19 09:50:44 -0700

    View Controller to display camera feed

---
+++ ARGameEngine/CameraViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  ARGameEngine
+//
+//  Created by Manjot Sangha on 2015-09-19.
+//  Copyright (c) 2015 Manjot Sangha. All rights reserved.
+//
+
+import Foundation
