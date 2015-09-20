21760f6285b892f685c75d4f02b90863e365d45b
msangha <manjot@mit.edu>
2015-09-19 23:46:31 -0400

    Update video display to use CIImage

--- ARGameEngine/CameraViewController.swift
+++ ARGameEngine/CameraViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
     private let captureSession = AVCaptureSession()
     private var captureDevice: AVCaptureDevice?
     private var previewLayer: AVCaptureVideoPreviewLayer?
+    //private let dataOutput = AVCaptureVideoDataOutput()
+    
+    
+    var imageView: UIImageView = UIImageView()
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -26,6 +30,9 @@ class CameraViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.frame = self.view.layer.frame
+        self.view.addSubview(imageView)
         
         captureSession.sessionPreset = AVCaptureSessionPresetHigh
         
@@ -45,16 +52,49 @@ class CameraViewController: UIViewController {
     
     private func beginSession() {
         var error: NSError?
-        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &error))
         
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &error))
+
         if (error != nil) {
             println("Error: \(error?.description)")
         }
         
+        let dataOutput = AVCaptureVideoDataOutput()
+        
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange]
+        dataOutput.setSampleBufferDelegate(self, queue: dispatch_get_main_queue())
+        
+        captureSession.addOutput(dataOutput)
+
+        captureSession.startRunning()
+        
+        /*
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        UIGraphicsBeginImageContext(previewLayer!.bounds.size)
+        previewLayer?.renderInContext(UIGraphicsGetCurrentContext())
+        //var cgImage: CGImageRef = CGBitmapContextCreateImage(UIGraphicsGetCurrentContext())
+        UIGraphicsEndImageContext();
+        
+        //var ciImage: CIImage = CIImage(CGImage: cgImage)
+        
         self.view.layer.addSublayer(previewLayer)
         previewLayer?.frame = self.view.layer.frame
         captureSession.startRunning()
+        */
+    }
+    
+    func mockChangeImage(image: CIImage) -> CIImage {
+        return image
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        var imageRotationMatrix = CGAffineTransformMakeRotation(CGFloat(-1*M_PI_2))
+        var ciImage: CIImage = CIImage(CVPixelBuffer: CMSampleBufferGetImageBuffer(sampleBuffer)).imageByApplyingTransform(imageRotationMatrix)
+        var newCIImage: CIImage = mockChangeImage(ciImage)
+        var uiImage: UIImage = UIImage(CIImage: newCIImage)!
+        imageView.image = uiImage
     }
     
 }
